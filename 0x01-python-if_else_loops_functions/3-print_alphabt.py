2d902bb6f1f2ee832b7c151a4649edbdfa90bb75
MELJOESTEIN <thierrymemel@gmail.com>
2023-01-03 21:21:37 +0000

    3-print_alphabt.py edited

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for ch in range(ord('a'), ord('z')+1):
+    if chr(ch) not in 'qe':
+        print("{:c}".format(ch), end='')
